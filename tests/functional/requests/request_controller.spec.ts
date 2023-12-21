fcc2c0a2bbdf15ea4dfe786dc8041519632e938b
Emjay6299 <emjay6299@gmail.com>
2023-12-21 11:16:19 +0100

    modified: fixed error response in request/test

--- tests/functional/requests/request_controller.spec.ts
+++ tests/functional/requests/request_controller.spec.ts
@@ -2,8 +2,16 @@ import { test } from '@japa/runner'
 
 test.group('Requests request controller', () => {
   test('submit a support request', async ({ client }) => {
-    const response = await client.post('/log-request')
+    const fields = {
+      firstName: 'John',
+      lastName: 'Doe',
+      emailAddress: 'john.doe@example.com',
+      messageTitle: 'Support Request',
+      messageBody: 'This is a test support request.',
+    }
 
+    const response = await client.post('/log-request').fields(fields)
+    response.assertStatus(200)
     console.log(response.body())
   })
 })
